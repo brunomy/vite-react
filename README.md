711a8dbc78c351242d961af62c8724edc7f6c927
bruno <brunopuc2015@gmail.com>
2024-08-28 16:23:22 -0300

    iniciando projeto e criando o primeiro componente

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # vite-react
 Projeto react utilizando vite
+
+##comandos iniciais
+npm create vite@latest
---
+++ app/src/componentes/Sidebar/index.tsx
@@ -0,0 +1,46 @@
+import Logo from './assets/Logo.svg';
+import Feed from './assets/feed.svg';
+import Perfil from './assets/account_circle.svg';
+import Info from './assets/info.svg';
+import Sair from './assets/logout.svg';
+
+
+
+export default function Sidebar(){
+    return (
+        <aside>
+            <img src={Logo} alt="Logo da aplicação" />
+            <nav>
+                <ul>
+                    <li>
+                        <a href="#">Publicar</a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src={Feed} alt="" />
+                            <span>Feed</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src={Perfil} alt="" />
+                            <span>Perfil</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src={Info} alt="" />
+                            <span>Sobre nós</span>
+                        </a>
+                    </li>
+                    <li>
+                        <a href="#">
+                            <img src={Sair} alt="" />
+                            <span>Sair</span>
+                        </a>
+                    </li>
+                </ul>
+            </nav>
+        </aside>
+    )
+}
