7bbfec39f3c8c51eee600462c02ac77c3a8fd36c
bruno <brunopuc2015@gmail.com>
2024-08-28 17:31:08 -0300

    add - estilização e criação de componente de pesquisa

---
+++ app/src/componentes/BarraDePesquisa/index.tsx
@@ -0,0 +1,7 @@
+import './styles.scss';
+
+export default function BarraDePesquisa() {
+    return (
+        <input className='barra-pesquisa' type="search" placeholder='Digite o que você procura' />
+    )
+}
--- app/src/componentes/Sidebar/index.tsx
+++ app/src/componentes/Sidebar/index.tsx
@@ -1,40 +1,39 @@
-import Logo from './assets/Logo.svg';
-import Feed from './assets/feed.svg';
-import Perfil from './assets/account_circle.svg';
-import Info from './assets/info.svg';
-import Sair from './assets/logout.svg';
+import Logo from "./assets/Logo.svg";
+import Feed from "./assets/feed.svg";
+import Perfil from "./assets/account_circle.svg";
+import Info from "./assets/info.svg";
+import Sair from "./assets/logout.svg";
+import './styles.scss'
 
-
-
-export default function Sidebar(){
+export default function Sidebar() {
     return (
         <aside>
             <img src={Logo} alt="Logo da aplicação" />
             <nav>
-                <ul>
+                <ul className="lista-sidebar">
                     <li>
-                        <a href="#">Publicar</a>
+                        <a href="#" className="item__link-publicacao">Publicar</a>
                     </li>
                     <li>
-                        <a href="#">
+                        <a href="#" className="item__link active">
                             <img src={Feed} alt="" />
                             <span>Feed</span>
                         </a>
                     </li>
                     <li>
-                        <a href="#">
+                        <a href="#" className="item__link">
                             <img src={Perfil} alt="" />
                             <span>Perfil</span>
                         </a>
                     </li>
                     <li>
-                        <a href="#">
+                        <a href="#" className="item__link">
                             <img src={Info} alt="" />
                             <span>Sobre nós</span>
                         </a>
                     </li>
                     <li>
-                        <a href="#">
+                        <a href="#" className="item__link">
                             <img src={Sair} alt="" />
                             <span>Sair</span>
                         </a>
@@ -42,5 +41,5 @@ export default function Sidebar(){
                 </ul>
             </nav>
         </aside>
-    )
+    );
 }
