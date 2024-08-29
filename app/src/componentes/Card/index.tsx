a81ca138fc3fa7dad2394e995d6193858c875bbb
bruno <brunopuc2015@gmail.com>
2024-08-29 14:34:13 -0300

    [projeto finalizado]

--- app/src/componentes/Card/index.tsx
+++ app/src/componentes/Card/index.tsx
@@ -1,41 +1,39 @@
 import './styles.scss';
-import Capa from './assets/capa.png';
 import Chat from './assets/chat.svg';
 import Code from './assets/code.svg';
 import Share from './assets/share.svg';
-import User from './assets/user.png';
 
-export default function Card() {
+export default function Card({id, imagemUrl, titulo, resumo, linhasDeCodigo, compartilhamentos, comentarios, usuario}) {
     return (
         <article className="card">
             <div className='card__image'>
-                <img src={Capa} alt="Imagem do post" />
+                <img src={imagemUrl} alt="Imagem do post" />
             </div>
             <div className="card__conteudo">
                 <div className="conteudo__texto">
-                    <h3>Titulo</h3>
-                    <p>resumo</p>
+                    <h3>{titulo}</h3>
+                    <p>{resumo}</p>
                 </div>
 
                 <div className="conteudo__rodape">
                     <ul>
                         <li>
                             <img src={Code} alt="codigos" />
-                            100
+                            {linhasDeCodigo}
                         </li>
                         <li>
                             <img src={Share} alt="Compartilhamentos" />
-                            12
+                            {compartilhamentos}
                         </li>
                         <li>
                             <img src={Chat} alt="Comentarios" />
-                            10
+                            {comentarios}
                         </li>
                     </ul>
                     
                     <div className="rodape__usuario">
-                        <img src={User} alt="imagem do usuraio" />
-                        @bruno
+                        <img src={usuario.imagem} alt="imagem do usuraio" />
+                        @{usuario.nome}
                     </div>
                 </div>
             </div>
